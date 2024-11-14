6a8948a379b746c24572a7e270b528470eb9b5ca
Miguel Angel Villegas <chumilo227@gmail.com>
2024-11-14 18:32:01 -0500

    pidiendo el nombre del usuario

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,22 @@
+import java.util.Scanner;
+
 public class Game {
-    static int vidas, movimientos, resultados, tiempo;
+
+
+    final Scanner entrada = new Scanner(System.in);
 
     GameBoard tablero;
     Player jugador;
 
     // Inicializa el tablero y el jugador en una posición aleatoria.
     public void empezarNuevoJuego(){
-        System.out.println("Empezando nuevo Juego");
+        int vidas, movimientos, resultados, tiempo;
+        jugador = new Player();
+
+        System.out.print("Ingresa tu nombre para comenzar: ");
+        String nombre = entrada.nextLine();
+        jugador.setNombre(nombre);
+        System.out.println("Su nombre es " + jugador.getNombre());
     }
 
     // Llama a la clase GameBoard para actualizar la posición del jugador según la dirección.
