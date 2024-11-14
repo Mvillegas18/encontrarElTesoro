1b8f89403737368b38b504ffbaefcf9a1e600eb8
Miguel Angel Villegas <chumilo227@gmail.com>
2024-11-14 18:30:35 -0500

    añadiendo atributos, getters y setters

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,38 @@
 public class Player {
+    String nombre;
+    int positionX;
+    int positionY;
+    int vidas;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+
+    public int getVidas() {
+        return vidas;
+    }
+
+    public void setVidas(int vidas) {
+        this.vidas = vidas;
+    }
 }
