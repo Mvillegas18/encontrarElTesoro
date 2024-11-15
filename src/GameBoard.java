2f7e7fc944b684e1acea23fe7997ed07e583b976
Miguel Angel Villegas <chumilo227@gmail.com>
2024-11-14 20:49:04 -0500

    creando le metodo de inicializar el tablero e imprimirlo, colocar al usuario en una posicion aleatoria

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,2 +1,42 @@
+import java.util.Random;
+
 public class GameBoard {
+
+    private final int TAMANIO = 40;
+    private final char[][] tablero;
+
+    Player jugador;
+
+    public GameBoard() {
+        tablero = new char[TAMANIO][TAMANIO];
+    }
+
+    public void inicializarTablero (){
+        for (int i = 0; i < TAMANIO; i++) {
+            for (int j = 0; j < TAMANIO; j++) {
+                tablero[i][j] = '.';
+            }
+        }
+
+        jugador = new Player();
+        Random random = new Random();
+
+        int randomX = random.nextInt(TAMANIO);
+        int randomY = random.nextInt(TAMANIO);
+        jugador.setPositionX(randomX);
+        jugador.setPositionY(randomY);
+
+        tablero[jugador.getPositionX()][jugador.getPositionY()] = 'J';
+
+        imprimirTablero();
+    }
+
+    private void imprimirTablero(){
+        for (int i = 0; i < TAMANIO; i++) {
+            for (int j = 0; j < TAMANIO; j++) {
+                System.out.print(tablero[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
 }
