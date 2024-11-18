538af08803d7f7f504170e9b464f35e899d5e770
Miguel Angel Villegas <chumilo227@gmail.com>
2024-11-18 17:01:15 -0500

    crear la implementacion del tablero

--- src/Result.java
+++ src/Result.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Result {
+    Player jugador;
+
+    public void resultado() {
+        List<String[]> resultados = new ArrayList <String[]> ();
+
+        jugador = new Player();
+        resultados.add(new String[] {jugador.getNombre(), jugador.getTiempoJugador(), jugador.getResultados()});
+
+        mostrarTabla(resultados);
+    }
+
+    public void mostrarTabla(List<String[]> resultados) {
+        // Imprimir encabezado
+        System.out.println("+-----------------+----------------+-----------+");
+        System.out.printf("| %-15s | %-14s | %-9s |\n", "Jugador", "Tiempo", "Resultado");
+        System.out.println("+-----------------+----------------+-----------+");
+
+        // Imprimir filas
+        for (String[] resultado : resultados) {
+            System.out.printf("| %-15s | %-14s | %-9s |\n", resultado[0], resultado[1], resultado[2]);
+        }
+
+        System.out.println("+-----------------+----------------+-----------+");
+    }
 }
